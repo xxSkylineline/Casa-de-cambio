8195be8c04dd917cb74a5d305987a19cc2a8181d
xxSkylineline <grayfx86@gmail.com>
2024-10-04 00:01:22 -0300

    Funcion de cargaValoresdeMonedas para mayor legilibilidad
    En el desarrollo de esta feature, se logro crear una mejora en la funcion para su lectura

--- src/script.js
+++ src/script.js
@@ -15,10 +15,11 @@ calendario.on('click',function(){
 
 $muestraMonedas.on('click', function(){
 
-    let $listaDivisas = $('#lista-conversion');
-    let $listaValorMonedas = $('#lista-valor-moneda');
 
-    $listaDivisas.text('')
+function cargarValoresDeMonedas(){
+
+    
+    let $listaValorMonedas = $('#listado-monedas');
     $listaValorMonedas.text('')
 
     fecha = calendario.val();
@@ -26,10 +27,21 @@ $muestraMonedas.on('click', function(){
     fetch(link+fecha+'?access_key='+access_key)
         .then(res => res.json())
         .then(resJSON => {
-            $('#base').text(`La base es: ${resJSON.base}`)
+            $('#base').text(`La base es: ${resJSON.base}`);
             Object.keys(resJSON.rates).forEach(monedas =>{
-                $('#lista-conversion').append($(`<li>${monedas}</li>`))
-                $('#lista-valor-moneda').append($(`<li>${resJSON.rates[monedas]}</li>`))
-            })
-        })
-})
+                let contenedorFila = $('<div></div>').addClass('row');
+                let contenedorColumna1 = $('<div></div>').addClass('col');
+                let contenedorColumna2 = $('<div></div>').addClass('col')
+                
+                $('#listado-monedas').append(
+                    contenedorFila
+                        .append(contenedorColumna1.text(monedas).addClass('moneda'))
+                        .append(contenedorColumna2.text(resJSON.rates[monedas]).addClass('valor-moneda'))
+
+                )
+                /* $('#lista-conversion').append($(`<li>${monedas}</li>`).addClass('moneda'));
+                $('#lista-valor-moneda').append($(`<li>${resJSON.rates[monedas]}</li>`).addClass('valor-moneda')); */
+            });
+        });
+}
+
